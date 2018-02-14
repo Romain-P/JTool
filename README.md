7fc52b8df11fd0d5376d14a354fc8bf405578de4
Romain <romain.pillot@epitech.eu>
2018-02-14 16:13:37 +0100

    added project base

---
+++ README.md
@@ -0,0 +1,5 @@
+##JTool
+
+A basic gui demo for a mate
+* Using Eclipse
+* Using WindowsBuilder pro
---
+++ src/org/jtool/core/Main.java
@@ -0,0 +1,59 @@
+package org.jtool.core;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		applyDefaultSystemLook();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setResizable(false);
+		setTitle("JTool");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/icon.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+	/**
+	 *  Apply the default system look e.g beautiful windows for WindowsOs.
+	 *  By default, a java ui application is taking built-in java css, that are for the most so old and ugly.
+	 */
+	private static void applyDefaultSystemLook() {
+		try {
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {}
+	}
+}
